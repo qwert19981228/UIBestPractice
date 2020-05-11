e062d0299d0254e147a46d87bf77bccdd64232c7
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-05-11 14:41:16 +0800

    Update MsgAdapter.kt

--- app/src/main/java/com/example/uibestpractice/MsgAdapter.kt
+++ app/src/main/java/com/example/uibestpractice/MsgAdapter.kt
@@ -7,13 +7,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.lang.IllegalArgumentException
 
-class MsgAdapter(val msgList: List<Msg>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    inner class LeftViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val leftMsg:TextView = view.findViewById(R.id.leftMsg)
-    }
-    inner class RightViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val rightMsg:TextView = view.findViewById(R.id.rightMsg)
-    }
+class MsgAdapter(val msgList: List<Msg>) : RecyclerView.Adapter<MsgViewHolder>() {
+
+
 
     override fun getItemViewType(position: Int): Int {
         val msg = msgList[position]
@@ -30,12 +26,11 @@ class MsgAdapter(val msgList: List<Msg>) : RecyclerView.Adapter<RecyclerView.Vie
 
     override fun getItemCount() = msgList.size
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int){
+    override fun onBindViewHolder(holder: MsgViewHolder, position: Int){
         val msg = msgList[position]
         when(holder){
             is LeftViewHolder -> holder.leftMsg.text = msg.content
             is RightViewHolder -> holder.rightMsg.text = msg.content
-            else -> throw IllegalArgumentException()
         }
     }
 }
